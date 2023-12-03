cd6ef9a0af1144e550d2cd9b7db1796d830a3f48
gnanasurya <gnanasurya2@gmail.com>
2023-12-03 19:50:05 +0530

    added benchmarks

--- day-01/target/debug/deps/part2.d
+++ day-01/target/debug/deps/part2.d
@@ -1,6 +1,6 @@
-D:\development\advent-of-code-2023\day-01\target\debug\deps\part2.exe: src\bin\part2.rs src\bin\./input2.txt
+D:\development\advent-of-code-2023\day-01\target\debug\deps\part2.exe: src\bin\part2.rs src\bin\../input1.txt
 
-D:\development\advent-of-code-2023\day-01\target\debug\deps\part2.d: src\bin\part2.rs src\bin\./input2.txt
+D:\development\advent-of-code-2023\day-01\target\debug\deps\part2.d: src\bin\part2.rs src\bin\../input1.txt
 
 src\bin\part2.rs:
-src\bin\./input2.txt:
+src\bin\../input1.txt:
--- day-01/target/debug/part2.d
+++ day-01/target/debug/part2.d
@@ -1,1 +1,1 @@
-D:\development\advent-of-code-2023\day-01\target\debug\part2.exe: D:\development\advent-of-code-2023\day-01\src\bin\input2.txt D:\development\advent-of-code-2023\day-01\src\bin\part2.rs
+D:\development\advent-of-code-2023\day-01\target\debug\part2.exe: D:\development\advent-of-code-2023\day-01\src\bin\part2.rs D:\development\advent-of-code-2023\day-01\src\input1.txt D:\development\advent-of-code-2023\day-01\src\lib.rs D:\development\advent-of-code-2023\day-01\src\part1.rs D:\development\advent-of-code-2023\day-01\src\part2.rs
